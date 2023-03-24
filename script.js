a0d58deda3f719fe35d823f8bfe6029ce517897e
drake <aracelitoran@gmail.com>
2023-03-24 11:52:02 -0500

    second commit, script.js changes

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Global scope variables // 
 
-let a = 'this is the beggining of the Real nigga role call generator'
-console.log(a)
+message1 = "If you woke up today, "
+message2 = "By the end of this month, you need to "
+message3 = " "
+
+elements1 = ["hit the gym!","get some bread!","be a real one"];
+elements2 = ["level up!","move silent.","stunt on em!"]
+elements3 = ["Ayyyy! Do ya thang bwa!","U need 2 get rite goofy!","It's MANY MEN that wish death upon you!"]
+
+array1 = [message1, message2, message3];
+array2 = [elements1, elements2, elements3];
+
+
+//functions//
+
